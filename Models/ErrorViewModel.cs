4694d7e80c5d14bb6c847d0e1a218918be4697d8
Max Guclu <m.guclu@live.se>
2023-08-29 19:57:32 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaxGuclu_Project1_Anonymous_forum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
